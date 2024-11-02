fae203c8abe850b77fe63b3abb4c859b0508c6f9
Jiho Kim <83708500+Jiho001@users.noreply.github.com>
2024-11-03 00:28:38 +0900

    Title: 삼항연산자, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241103/삼항연산자/ternary-operator.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+        String grade = score==100 ? "pass":"fail";
+        System.out.print(grade);
+    }
+}
