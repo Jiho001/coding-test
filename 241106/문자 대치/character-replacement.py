f5ac0cda610604a562a5e87a9a6e2ccdb2cd4c42
Jiho Kim <83708500+Jiho001@users.noreply.github.com>
2024-11-06 01:39:10 +0900

    Title: 문자 대치, Time: 42ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241106/문자 대치/character-replacement.py
@@ -0,0 +1,12 @@
+def solution(x):
+    if '.' in x: sep_point_list_len = list(map(len, x.split('.')))
+    else: sep_point_list_len = [len(x)]
+    return_list = []
+    for l in sep_point_list_len:
+        if l % 2 == 1: return -1
+        else:
+            if l % 4 == 0: return_list.append("a"*4*(l//4))
+            elif l % 4 == 2: return_list.append("a"*4*(l//4) + "b"*2)
+    return ''.join(return_list)
+
+print(solution(input()))
