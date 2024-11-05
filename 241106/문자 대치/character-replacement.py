83649372e88fc4b73306f4a4b3923d820c549f09
Jiho Kim <83708500+Jiho001@users.noreply.github.com>
2024-11-06 01:49:24 +0900

    Title: 문자 대치, Time: 43ms, Memory: 15MB, Status: Passed - CodeTree

--- 241106/문자 대치/character-replacement.py
+++ 241106/문자 대치/character-replacement.py
@@ -4,11 +4,12 @@ def solution(x):
         else: return -1
     else: sep_point_list_len = [len(x)]
     return_list = []
-    for l in sep_point_list_len:
+    for e, l in enumerate(sep_point_list_len):
         if l % 2 == 1: return -1
         else:
             if l % 4 == 0: return_list.append("a"*4*(l//4))
             elif l % 4 == 2: return_list.append("a"*4*(l//4) + "b"*2)
+            if e != len(sep_point_list_len) - 1: return_list.append('.')
     return ''.join(return_list)
 
 print(solution(input()))
