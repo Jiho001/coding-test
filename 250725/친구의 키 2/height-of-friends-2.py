c77229a47ef83760aab570d6d2b71a736113a06a
Jiho Kim <83708500+Jiho001@users.noreply.github.com>
2025-07-25 22:00:37 +0900

    Title: 친구의 키 2, Time: 177ms, Memory: 24MB, Status: Passed - Codetree

---
+++ 250725/친구의 키 2/height-of-friends-2.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+n, m = tuple(map(int, input().split()))
+edges = [[] for _ in range(n + 1)]
+
+indegree = [0] * (n + 1)
+q = deque()
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+
+    edges[x].append(y) 
+    indegree[y] += 1 
+for i in range(1, n + 1):
+    if not indegree[i]:
+        q.append(i)
+cnt = 0
+while q:
+    x = q.popleft()
+    cnt += 1
+    for y in edges[x]:
+        indegree[y] -= 1
+        if not indegree[y]:
+            q.append(y)
+if cnt == n:
+    print("Consistent")
+else:
+    print("Inconsistent")
